9e71b5f12606db250f871914874563e8137feddb
unknown <jhallett@CND4215JNW.campus.ltu.edu>
2016-02-02 20:41:37 -0500

    Action code for shield

---
+++ Tanks/Assets/Shield.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Shield : MonoBehaviour
+{
+	
+	// Update is called once per frame
+	void Update ()
+    {
+	    
+	}
+
+
+
+    void OnTriggerEnter2D.Collider2D(Collider "Missle")
+    {
+
+    }
+
+}
