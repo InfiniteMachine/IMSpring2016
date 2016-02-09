46e1503140f639f063ae9de5a71520c30e41261b
imburnis <jkradatz@gmail.com>
2016-02-09 15:23:07 -0500

    Scaling camera follow

---
+++ Tanks/Assets/Editor/CamFollowEditor.cs
@@ -0,0 +1,91 @@
+﻿#if UNITY_EDITOR
+using UnityEngine;
+using UnityEditor;
+using System.Collections;
+
+[CustomEditor(typeof(CamFollow))]
+public class CamFollowEditor : Editor
+{
+    private CamFollow c;
+    private bool backgroundObject;
+
+    [MenuItem("Custom 2D/Add CamFollow")]
+    static void camFollow(MenuCommand menuCommand)
+    {
+        Undo.RecordObject(Camera.main, "Add CamFollow to MainCamera.");
+        Camera.main.gameObject.AddComponent<CamFollow>();
+    }
+
+    void OnEnable()
+    {
+        c = (CamFollow)target;
+    }
+
+    void OnSceneGUI()
+    {
+        float left = c.leftBounds,
+              right = c.rightBounds,
+              up = c.upperBounds,
+              down = c.lowerBounds,
+              margin = c.margin;
+
+        Handles.DrawSolidRectangleWithOutline(new Vector3[] { new Vector3(left, up), new Vector3(right, up), new Vector3(right, down), new Vector3(left, down), },
+            new Color(0, 0, 0, 0.15f), new Color(255, 0, 255));
+        Handles.color = new Color(255, 0, 255);
+
+        Vector2 corner = Handles.FreeMoveHandle(new Vector2(left, up), Quaternion.identity, HandleUtility.GetHandleSize(new Vector2(left, up)) * 0.05f, Vector3.zero, Handles.DotCap);
+        left = corner.x;
+        up = corner.y;
+
+        corner = Handles.FreeMoveHandle(new Vector2(right, up), Quaternion.identity, HandleUtility.GetHandleSize(new Vector2(right, up)) * 0.05f, Vector3.zero, Handles.DotCap);
+        right = corner.x;
+        up = corner.y;
+
+        corner = Handles.FreeMoveHandle(new Vector2(right, down), Quaternion.identity, HandleUtility.GetHandleSize(new Vector2(right, up)) * 0.05f, Vector3.zero, Handles.DotCap);
+        right = corner.x;
+        down = corner.y;
+
+        corner = Handles.FreeMoveHandle(new Vector2(left, down), Quaternion.identity, HandleUtility.GetHandleSize(new Vector2(left, down)) * 0.05f, Vector3.zero, Handles.DotCap);
+        left = corner.x;
+        down = corner.y;
+
+        left = Handles.Slider(new Vector2(left, (up + down) * 0.5f), -Vector2.right, HandleUtility.GetHandleSize(new Vector2(right, up)) * 0.05f, Handles.DotCap, 0).x;
+        right = Handles.Slider(new Vector2(right, (up + down) * 0.5f), Vector2.right, HandleUtility.GetHandleSize(new Vector2(right, up)) * 0.05f, Handles.DotCap, 0).x;
+
+        up = Handles.Slider(new Vector2((right + left) * 0.5f, up), Vector2.up, HandleUtility.GetHandleSize(new Vector2(right, up)) * 0.05f, Handles.DotCap, 0).y;
+        down = Handles.Slider(new Vector2((right + left) * 0.5f, down), -Vector2.up, HandleUtility.GetHandleSize(new Vector2(right, up)) * 0.05f, Handles.DotCap, 0).y;
+
+        if (right < left)
+        {
+            float temp = right;
+            right = left;
+            left = temp;
+        }
+        if (up < down)
+        {
+            float temp = up;
+            up = down;
+            down = temp;
+        }
+
+        if (GUI.changed)
+        {
+            Undo.RecordObject(c, "CamFollow: Resize Map Boundries.");
+            c.leftBounds = left;
+            c.rightBounds = right;
+            c.upperBounds = up;
+            c.lowerBounds = down;
+            EditorUtility.SetDirty(target);
+        }
+
+        if (GUI.changed)
+        {
+            Undo.RecordObject(c, "CamFollow: Change Object Follow Margin.");
+            c.margin = margin;
+            EditorUtility.SetDirty(target);
+        }
+
+        Handles.color = Color.white;
+    }
+}
+#endif
---
+++ Tanks/Assets/Scripts/CamFollow.cs
@@ -0,0 +1,107 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+
+public class CamFollow : MonoBehaviour
+{
+    public static CamFollow instance;
+    
+    public float margin = 2f;
+    public float moveSpeed = 3f;
+    public float orthoScaleSpeed = 1f;
+    [HideInInspector]
+    public float leftBounds = -5;
+    [HideInInspector]
+    public float rightBounds = 5;
+    [HideInInspector]
+    public float upperBounds = 5;
+    [HideInInspector]
+    public float lowerBounds = -5;
+    
+    private float screenWorldWidth;
+    private float screenWorldHeight;
+    private Vector3 lastPosition;
+    private float lastOrtographicSize = 5f;
+
+    private List<Transform> visibleObjects;
+
+    void Awake()
+    {
+        instance = this;
+    }
+
+    // Use this for initialization
+    void Start()
+    {
+        visibleObjects = new List<Transform>();
+        foreach (GameObject go in GameObject.FindGameObjectsWithTag("Player"))
+            visibleObjects.Add(go.transform);
+        foreach (GameObject go in GameObject.FindGameObjectsWithTag("Baton"))
+            visibleObjects.Add(go.transform);
+
+        lastPosition = transform.position;
+        lastOrtographicSize = Camera.main.orthographicSize;
+        CalculateWindowResize();
+        ConstrainCameraToView();
+        
+    }
+
+    // Update is called once per frame
+    void LateUpdate()
+    {
+        //Generate camera bounds
+        Rect r = GenerateRect();
+        float newOrtho = Mathf.Max((r.height) / 2, (r.width) / (2 * Camera.main.aspect));
+        Camera.main.orthographicSize = Mathf.MoveTowards(Camera.main.orthographicSize, newOrtho, orthoScaleSpeed * Time.deltaTime);
+        Vector3 newPosition = new Vector3(r.x + (r.width / 2), r.y + (r.height / 2), Camera.main.transform.position.z);
+        Camera.main.transform.position = Vector3.MoveTowards(Camera.main.transform.position, newPosition, moveSpeed * Time.deltaTime);
+        ConstrainCameraToView();
+    }
+
+    private Rect GenerateRect()
+    {
+        Rect r = new Rect();
+        r.yMax = r.yMin = visibleObjects[0].position.y;
+        r.xMax = r.xMin = visibleObjects[0].position.x;
+        for(int i = 1; i < visibleObjects.Count; i++)
+        {
+            Vector3 pos = visibleObjects[i].position;
+            if (pos.y < r.yMin)
+                r.yMin = pos.y;
+            else if (pos.y > r.yMax)
+                r.yMax = pos.y;
+            if (pos.x < r.xMin)
+                r.xMin = pos.x;
+            else if (pos.x > r.xMax)
+                r.xMax = pos.x;
+        }
+        r.yMax = Mathf.Min(r.yMax + margin, upperBounds);
+        r.yMin = Mathf.Max(r.yMin - margin, lowerBounds);
+        r.xMax = Mathf.Min(r.xMax + margin, rightBounds);
+        r.xMin = Mathf.Max(r.xMin - margin, leftBounds);
+        return r;
+    }
+
+    private void ConstrainCameraToView()
+    {
+        CalculateWindowResize();
+        if (lastPosition != transform.position)
+        {
+            lastPosition.x = Mathf.Clamp(transform.position.x, leftBounds + screenWorldWidth, rightBounds - screenWorldWidth);
+            lastPosition.y = Mathf.Clamp(transform.position.y, lowerBounds + screenWorldHeight, upperBounds - screenWorldHeight);
+            transform.position = lastPosition;
+        }
+    }
+
+    public void CalculateWindowResize()
+    {
+        if (lastOrtographicSize != Camera.main.orthographicSize)
+        {
+            float rightBorder = Camera.main.ViewportToWorldPoint(new Vector3(1, 0, 0)).x;
+            float topBorder = Camera.main.ViewportToWorldPoint(new Vector3(0, 1, 0)).y;
+
+            screenWorldHeight = topBorder - transform.position.y;
+            screenWorldWidth = rightBorder - transform.position.x;
+            lastOrtographicSize = Camera.main.orthographicSize;
+        }
+    }
+}
